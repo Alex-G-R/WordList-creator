4efede5415fbafb92b52b0e001ae2889967c81f9
Alex <alex.grabski555@gmail.com>
2023-09-24 19:51:19 +0200

    Merging getData and extractDOB to one function

--- functions/getData.js
+++ functions/getData.js
@@ -3,15 +3,65 @@ function getData (key, dob, keyA, keyB, keyC) {
     if( key == ""){
         throw console.error("You didn't provide the main key");
     }
+
+    let monthString;
     if( dob == ""){
         dob = false;
+        year = false;
+        month = false;
+        monthString = false;
+        day = false;
     } else if (dob.length == 10){
         year = dob.substr(0,4); // extract the year
         month = dob.substr(5,2); // extract the month
         day = dob.substr(8,2); // extract the day
+
+        switch(month) {
+            case "01":
+                monthString = "January"
+                break;
+            case "02":
+                monthString = "February"
+                break;
+            case "03":
+                monthString = "March"
+                break;
+            case "04":
+                monthString = "April"
+                break;
+            case "05":
+                monthString = "May"
+                break;
+            case "06":
+                monthString = "June"
+                break;
+            case "07":
+                monthString = "July"
+                break;
+            case "08":
+                monthString = "August"
+                break;
+            case "09":
+                monthString = "September"
+                break;
+            case "10":
+                monthString = "October"
+                break;
+            case "11":
+                monthString = "November"
+                break;
+            case "12":
+                monthString = "December"
+                break;
+            default:
+                monthString = false;
+                
+        }
+
     } else{
         throw console.error("You provided wrongly formated DOB")
     }
+
     if( keyA == ""){
         keyA = false;
     }
@@ -24,10 +74,14 @@ function getData (key, dob, keyA, keyB, keyC) {
 
     const $DATA = {
         "keyWord": key,
-        "dob": dob,
         "keyA": keyA,
         "keyB": keyB,
-        "keyC": keyC
+        "keyC": keyC,
+        "dob": dob,
+        "year": year,
+        "month": month,
+        "monthString": monthString,
+        "day": day,
     }
 
     return $DATA
