24d2f7da275cf4002e2eb5a60f3b693f67cac56c
Alex <alex.grabski555@gmail.com>
2023-09-24 17:01:09 +0200

    get the data

--- app.js
+++ app.js
@@ -1,2 +1,60 @@
 const prompt = require('prompt-sync')({sigint: true});
 
+//declare constants
+let $KEY = "";
+let $DOB = "";
+let $KEYc = "";
+let $KEYb = "";
+let $KEYa = "";
+
+// Declare functions to get the data
+// key word 
+function getTheMainKey(){
+    $KEY = prompt("Provide the key value for example name: ");
+}
+
+// date of birth - DOB
+function getTheDOB(){
+    $DOB = prompt("Provide the date of birth YYYY/MM/DD if you don't know leave blank and press ENTER: ");
+}
+
+// additional keywords
+function getTheAdditionalKeyA(){
+    $KEYa = prompt("Provide first additional key word if not needed leave blank and press ENTER: ");
+}
+function getTheAdditionalKeyB(){
+    $KEYb = prompt("Provide second additional key word if not needed leave blank and press ENTER: ");
+}
+function getTheAdditionalKeyC(){
+    $KEYc = prompt("Provide third additional key word if not needed leave blank and press ENTER: ");
+}
+
+// Get the data
+getTheMainKey();
+getTheDOB();
+getTheAdditionalKeyA();
+getTheAdditionalKeyB();
+getTheAdditionalKeyC();
+
+// Check if blank
+while( $KEY === "" ){
+    getTheMainKey();
+}
+
+if( $DOB == "" ){
+    $DOB = false;
+}
+
+if( $KEYa === "" ){
+    $KEYa = false;
+}
+
+if( $KEYb === "" ){
+    $KEYb = false;
+}
+
+if( $KEYc === "" ){
+    $KEYc = false;
+}
+
+// sumarize the data
