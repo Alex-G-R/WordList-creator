df4abd2012dd4daca853d9c065fb9b828e53da77
Alex <alex.grabski555@gmail.com>
2023-09-25 10:54:26 +0200

    adding 5th generation

---
+++ functions/fifthGeneration.js
@@ -0,0 +1,176 @@
+
+function fourthGeneration (data, specialChars){
+    const fs = require('fs');
+
+    // MKW + year month day
+    if( data.dob != false){
+        const password = data.keyWord + data.year + data.month + data.day;
+        console.log(password);
+        fs.writeFileSync(`./wordLists/${data.keyWord}`+"List"+`.txt`, password + "\n", {flag: "a"}) 
+        for(let i = 0; i < specialChars.length; i++){
+
+            const password = data.keyWord + data.year + data.month + data.day + specialChars[i];
+            console.log(password);
+            fs.writeFileSync(`./wordLists/${data.keyWord}`+"List"+`.txt`, password + "\n", {flag: "a"})
+
+        }
+    }
+
+    // MKW + year month
+    if( data.dob != false){
+        const password = data.keyWord + data.year + data.month;
+        console.log(password);
+        fs.writeFileSync(`./wordLists/${data.keyWord}`+"List"+`.txt`, password + "\n", {flag: "a"}) 
+        for(let i = 0; i < specialChars.length; i++){
+
+            const password = data.keyWord + data.year + data.month + specialChars[i];
+            console.log(password);
+            fs.writeFileSync(`./wordLists/${data.keyWord}`+"List"+`.txt`, password + "\n", {flag: "a"})
+
+        }
+    }
+
+    // MKW + year
+    if( data.dob != false){
+        const password = data.keyWord + data.year;
+        console.log(password);
+        fs.writeFileSync(`./wordLists/${data.keyWord}`+"List"+`.txt`, password + "\n", {flag: "a"}) 
+        for(let i = 0; i < specialChars.length; i++){
+
+            const password = data.keyWord + data.year + specialChars[i];
+            console.log(password);
+            fs.writeFileSync(`./wordLists/${data.keyWord}`+"List"+`.txt`, password + "\n", {flag: "a"})
+
+        }
+    }
+
+    // MKW + month day
+    if( data.dob != false){
+        const password = data.keyWord + data.month + data.day;
+        console.log(password);
+        fs.writeFileSync(`./wordLists/${data.keyWord}`+"List"+`.txt`, password + "\n", {flag: "a"}) 
+        for(let i = 0; i < specialChars.length; i++){
+
+            const password = data.keyWord + data.month + data.day + specialChars[i];
+            console.log(password);
+            fs.writeFileSync(`./wordLists/${data.keyWord}`+"List"+`.txt`, password + "\n", {flag: "a"})
+
+        }
+    }
+
+    // MKW + month
+    if( data.dob != false){
+        const password = data.keyWord + data.month;
+        console.log(password);
+        fs.writeFileSync(`./wordLists/${data.keyWord}`+"List"+`.txt`, password + "\n", {flag: "a"}) 
+        for(let i = 0; i < specialChars.length; i++){
+
+            const password = data.keyWord + data.month + specialChars[i];
+            console.log(password);
+            fs.writeFileSync(`./wordLists/${data.keyWord}`+"List"+`.txt`, password + "\n", {flag: "a"})
+
+        }
+    }
+
+    // MKW + day
+    if( data.dob != false){
+        const password = data.keyWord + data.day;
+        console.log(password);
+        fs.writeFileSync(`./wordLists/${data.keyWord}`+"List"+`.txt`, password + "\n", {flag: "a"}) 
+        for(let i = 0; i < specialChars.length; i++){
+
+            const password = data.keyWord + data.day + specialChars[i];
+            console.log(password);
+            fs.writeFileSync(`./wordLists/${data.keyWord}`+"List"+`.txt`, password + "\n", {flag: "a"})
+
+        }
+    }
+
+
+    // MKW + year day
+    if( data.dob != false){
+        const password = data.keyWord + data.year + data.day;
+        console.log(password);
+        fs.writeFileSync(`./wordLists/${data.keyWord}`+"List"+`.txt`, password + "\n", {flag: "a"}) 
+        for(let i = 0; i < specialChars.length; i++){
+
+            const password = data.keyWord + data.year + data.day + specialChars[i];
+            console.log(password);
+            fs.writeFileSync(`./wordLists/${data.keyWord}`+"List"+`.txt`, password + "\n", {flag: "a"})
+
+        }
+    }
+
+    // MKW + day month
+    if( data.dob != false){
+        const password = data.keyWord + data.day + data.month;
+        console.log(password);
+        fs.writeFileSync(`./wordLists/${data.keyWord}`+"List"+`.txt`, password + "\n", {flag: "a"}) 
+        for(let i = 0; i < specialChars.length; i++){
+
+            const password = data.keyWord + data.day + data.month + specialChars[i];
+            console.log(password);
+            fs.writeFileSync(`./wordLists/${data.keyWord}`+"List"+`.txt`, password + "\n", {flag: "a"})
+
+        }
+    }
+
+    // MKW + day year
+    if( data.dob != false){
+        const password = data.keyWord + data.day + data.year;
+        console.log(password);
+        fs.writeFileSync(`./wordLists/${data.keyWord}`+"List"+`.txt`, password + "\n", {flag: "a"}) 
+        for(let i = 0; i < specialChars.length; i++){
+
+            const password = data.keyWord + data.day + data.year + specialChars[i];
+            console.log(password);
+            fs.writeFileSync(`./wordLists/${data.keyWord}`+"List"+`.txt`, password + "\n", {flag: "a"})
+
+        }
+    }
+
+    // MKW + day month year
+    if( data.dob != false){
+        const password = data.keyWord + data.day + data.month + data.year;
+        console.log(password);
+        fs.writeFileSync(`./wordLists/${data.keyWord}`+"List"+`.txt`, password + "\n", {flag: "a"}) 
+        for(let i = 0; i < specialChars.length; i++){
+
+            const password = data.keyWord + data.day + data.month + data.year + specialChars[i];
+            console.log(password);
+            fs.writeFileSync(`./wordLists/${data.keyWord}`+"List"+`.txt`, password + "\n", {flag: "a"})
+
+        }
+    }
+
+    // MKW + month day year
+    if( data.dob != false){
+        const password = data.keyWord + data.month + data.day + data.year;
+        console.log(password);
+        fs.writeFileSync(`./wordLists/${data.keyWord}`+"List"+`.txt`, password + "\n", {flag: "a"}) 
+        for(let i = 0; i < specialChars.length; i++){
+
+            const password = data.keyWord + data.month + data.day + data.year + specialChars[i];
+            console.log(password);
+            fs.writeFileSync(`./wordLists/${data.keyWord}`+"List"+`.txt`, password + "\n", {flag: "a"})
+
+        }
+    }
+
+    // MKW + year day month
+    if( data.dob != false){
+        const password = data.keyWord + data.year + data.day + data.month;
+        console.log(password);
+        fs.writeFileSync(`./wordLists/${data.keyWord}`+"List"+`.txt`, password + "\n", {flag: "a"}) 
+        for(let i = 0; i < specialChars.length; i++){
+
+            const password = data.keyWord + data.year + data.day + data.month + specialChars[i];
+            console.log(password);
+            fs.writeFileSync(`./wordLists/${data.keyWord}`+"List"+`.txt`, password + "\n", {flag: "a"})
+
+        }
+    }
+    
+}
+
+module.exports = fourthGeneration;
